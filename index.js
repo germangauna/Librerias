7fabe33df49b80b2e33f99a1977602c482268b6c
GermanG <germangauna88@gmail.com>
2024-07-18 21:14:43 -0300

    creamos boton con alert

---
+++ index.js
@@ -0,0 +1,114 @@
+// lo primero que vamos hacer con las librerias va a ser buscar en el navegador,
+//para instalar, en la pagina va a tener muchos ejemplos de alertas, copiamos el codigo
+// que nos da la pagina, copiamos y pegamos, dentro de los ejemplo hay muchas alertas.
+//
+//Para instalar lo que hay que hacer es ir hasta el final de la pagina donde dice install:
+
+//pegamos el link al principo del html.
+// y el script al final del body, junto con el script de js vinmculado, y asi podremos usar la libreria copiando codigo,
+// y agregandolo a nuestros proyectos.
+
+//<link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
+//{/* <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script> */}
+
+// con el script pegado al final ya tenemos la libreria vinculada
+// para saber si funciona pegaremos un codigo de alerta a ver si sale cuando levantamos la pagina.
+
+
+Swal.fire({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success"
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
